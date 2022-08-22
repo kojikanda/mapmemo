197aebed397fd67cf32a156344af0947a337b38e
kojikanda <yngwie_wanchi@yahoo.co.jp>
2022-08-22 16:22:54 +0900

    Application Source changed to get from GitHub

--- prepare_django.sh
+++ prepare_django.sh
@@ -1,12 +1,12 @@
 # DBデータ初期化
-docker-compose -f docker-compose.yml exec python python portfolio1/manage.py flush --no-input
+docker-compose -f docker-compose.yml exec python python manage.py flush --no-input
 
 # マイグレーション
-docker-compose -f docker-compose.yml exec python python portfolio1/manage.py makemigrations
-docker-compose -f docker-compose.yml exec python python portfolio1/manage.py migrate
+docker-compose -f docker-compose.yml exec python python manage.py makemigrations
+docker-compose -f docker-compose.yml exec python python manage.py migrate
 
 # staticファイル集約化
-docker-compose -f docker-compose.yml exec python python portfolio1/manage.py collectstatic --no-input --clear
+docker-compose -f docker-compose.yml exec python python manage.py collectstatic --no-input --clear
 
 # Djangoの管理ユーザ登録
-docker-compose -f docker-compose.yml exec python python portfolio1/manage.py createsuperuser
+docker-compose -f docker-compose.yml exec python python manage.py createsuperuser
